16860951990ddda48b11b80b9804ecf7554fd8e8
Richard Collins <the.hungarian@gmail.com>
2016-12-17 13:29:08 +0000

    Initial commit of best guess for cube i7

---
+++ android_cube_i7.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2014 The Android-x86 Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+$(call inherit-product,$(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# includes the base of Android-x86 platform
+$(call inherit-product,device/generic/common/x86_64.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+
+# Overrides
+PRODUCT_NAME := cm_android_cube_i7
+PRODUCT_BRAND := Android-x86
+PRODUCT_DEVICE := android_cube_i7
+PRODUCT_MODEL := Cube i7 Android-x86_64
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_LOCALES += en_GB
+PRODUCT_MANUFACTURER := cube
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.radio.noril=1 \
+    ro.dalvik.vm.isa.arm64=x86_64 \
+	ro.enable.native.bridge.exec64=1
---
+++ cm.mk
@@ -0,0 +1,5 @@
+# Release name
+PRODUCT_RELEASE_NAME := android_cube_i7
+
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/android_x86_64.mk)
