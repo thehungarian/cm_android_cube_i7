0730ec7757d22686d1c34085000fe73910c1e752
Richard Collins <the.hungarian@gmail.com>
2017-06-09 07:39:16 +0100

    made build nougat specific

--- docker/start.sh
+++ docker/start.sh
@@ -1,2 +1,2 @@
-docker build -t android_build .
-docker run -it --rm -v /home/mach/code/android-x86:/home/mach/code/android-x86 -u mach android_build
+docker build -t android_build_nougat .
+docker run -it --rm -v /home/mach/code/android-x86:/home/mach/code/android-x86 -u mach android_build_nougat
