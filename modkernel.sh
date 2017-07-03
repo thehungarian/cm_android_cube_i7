4d9e5a2fb13ff27ad581b91b901b9d2ac9d8d968
Richard Collins <the.hungarian@gmail.com>
2017-07-03 10:48:48 +0100

    Experimental kernel config

--- modkernel.sh
+++ modkernel.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-pushd ${ANDROID_BUILD_TOP}
 
 TARGET_KERNEL_SOURCE=kernel
 TARGET_KERNEL_CONFIG=android-cube_i7_defconfig
 
-${TARGET_KERNEL_SOURCE}/scripts/kconfig/merge_config.sh -m -n \
-                kernel/arch/x86/configs/android-x86_64_defconfig \
-                device/cube/cubei7/${TARGET_KERNEL_CONFIG} 
+cp ${TARGET_KERNEL_CONFIG} $OUT/obj/kernel/.config
 
-make -C kernel O=$OUT/obj/kernel ARCH=x86 menuconfig
+pushd ${ANDROID_BUILD_TOP}
+
+#${TARGET_KERNEL_SOURCE}/scripts/kconfig/merge_config.sh -m -n \
+#                kernel/arch/x86/configs/android-x86_64_defconfig \
+#                device/cube/cubei7/${TARGET_KERNEL_CONFIG} 
+
+
+make -C kernel O=$OUT/obj/kernel ARCH=x86_64 xconfig
 
 popd
 
-grep -v '^\#' ${ANDROID_BUILD_TOP}/kernel/arch/x86/configs/android-x86_64_defconfig | sort > orig.config && \
-grep -v '^\#' $OUT/obj/kernel/.config | sort > new.config && \
-diff --unchanged-line-format="" --old-line-format="" --new-line-format='%L' orig.config new.config > android-cube_i7_defconfig
+#grep -v '^\#' ${ANDROID_BUILD_TOP}/kernel/arch/x86/configs/android-x86_64_defconfig | sort > orig.config && \
+#grep -v '^\#' $OUT/obj/kernel/.config | sort > new.config && \
+#diff --unchanged-line-format="" --old-line-format="" --new-line-format='%L' orig.config new.config > ${TARGET_KERNEL_CONFIG}
+cp $OUT/obj/kernel/.config ${TARGET_KERNEL_CONFIG}
 
-rm orig.config 
-rm new.config
+#rm orig.config 
+#rm new.config
